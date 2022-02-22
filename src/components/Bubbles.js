1c7a17196771a705a8d4dcd329c79b6e12820c5e
Mattias <mattias.basso@gmail.com>
2022-02-22 19:17:59 +0100

    axes as svg lines

--- src/components/Bubbles.js
+++ src/components/Bubbles.js
@@ -15,8 +15,12 @@ export function Bubbles({ width, height }) {
   const yScale = d3.scaleLinear().domain([20, -20]).range([0, height])
   const circleScale = d3.scaleSqrt().domain([1, 20]).range([10, 50])
 
+  //   const axesY = d3.axisBottom(xScale)
+
   return (
     <svg width={width} height={height}>
+      <line x1="0" y1={height / 2} x2={width} y2={height / 2} stroke="#BDBDBD" opacity={0.8} />
+      <line x1={width / 2} y1={0} x2={width / 2} y2={height} stroke="#BDBDBD" opacity={0.8} />
       {bubbleData.map((d, i) => (
         <g key={i}>
           <circle
