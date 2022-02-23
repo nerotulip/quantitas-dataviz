27b5023e4a1aeafff6d3530c7f32162aefe837c2
Mattias <mattias.basso@gmail.com>
2022-02-23 13:45:49 +0100

    rect to bubble interaction

--- src/components/PCViz.js
+++ src/components/PCViz.js
@@ -18,17 +18,17 @@ export function PCViz({ width, height }) {
     {
       topic: 1,
       words: [
-        { word: 'car', count: Math.floor(Math.random() * 100) },
-        { word: 'white', count: Math.floor(Math.random() * 100) },
-        { word: 'great', count: Math.floor(Math.random() * 100) },
-        { word: 'gin', count: Math.floor(Math.random() * 100) },
-        { word: 'new york', count: Math.floor(Math.random() * 100) },
-        { word: 'daisy', count: Math.floor(Math.random() * 100) },
-        { word: 'gatsby', count: Math.floor(Math.random() * 100) },
-        { word: 'boat', count: Math.floor(Math.random() * 100) },
-        { word: 'lake', count: Math.floor(Math.random() * 100) },
-        { word: 'mountain', count: Math.floor(Math.random() * 100) },
-        { word: 'states', count: Math.floor(Math.random() * 100) },
+        { word: 'car', count: 24 },
+        { word: 'white', count: 36 },
+        { word: 'great', count: 12 },
+        { word: 'gin', count: 6 },
+        { word: 'new york', count: 30 },
+        { word: 'daisy', count: 87 },
+        { word: 'gatsby', count: 74 },
+        { word: 'boat', count: 23 },
+        { word: 'lake', count: 24 },
+        { word: 'mountain', count: 91 },
+        { word: 'states', count: 45 },
       ],
     },
   ]
@@ -37,18 +37,18 @@ export function PCViz({ width, height }) {
     {
       topic: 'default',
       words: [
-        { word: 'car', count: Math.floor(Math.random() * 100), topic: 1 },
-        { word: 'white', count: Math.floor(Math.random() * 100), topic: 2 },
-        { word: 'great', count: Math.floor(Math.random() * 100), topic: 3 },
-        { word: 'gin', count: Math.floor(Math.random() * 100), topic: 4 },
-        { word: 'new york', count: Math.floor(Math.random() * 100), topic: 5 },
-        { word: 'daisy', count: Math.floor(Math.random() * 100), topic: 6 },
-        { word: 'gatsby', count: Math.floor(Math.random() * 100), topic: 1 },
-        { word: 'boat', count: Math.floor(Math.random() * 100), topic: 2 },
-        { word: 'lake', count: Math.floor(Math.random() * 100), topic: 3 },
-        { word: 'mountain', count: Math.floor(Math.random() * 100), topic: 4 },
-        { word: 'states', count: Math.floor(Math.random() * 100), topic: 5 },
-        { word: 'nicolas', count: Math.floor(Math.random() * 100), topic: 5 },
+        { word: 'car', count: 33, topic: 1 },
+        { word: 'white', count: 33, topic: 2 },
+        { word: 'great', count: 33, topic: 3 },
+        { word: 'gin', count: 33, topic: 4 },
+        { word: 'new york', count: 33, topic: 5 },
+        { word: 'daisy', count: 33, topic: 6 },
+        { word: 'gatsby', count: 33, topic: 1 },
+        { word: 'boat', count: 33, topic: 2 },
+        { word: 'lake', count: 33, topic: 3 },
+        { word: 'mountain', count: 33, topic: 4 },
+        { word: 'states', count: 33, topic: 5 },
+        { word: 'nicolas', count: 33, topic: 5 },
       ],
     },
   ]
@@ -84,40 +84,7 @@ export function PCViz({ width, height }) {
   console.log(xScaleBars(87))
   return (
     // BARS DIV
-    <div>
-      <div>
-        <BarTitle>Top 30 Most Salient terms</BarTitle>
-        <svg width={width} height={height}>
-          {defaultData.map((d) =>
-            d.words.map((w, i) => (
-              <g key={i}>
-                <rect
-                  x={margins.left + 15}
-                  y={yScaleBars(w.word)}
-                  width={xScaleBars(w.count)}
-                  height={10}
-                  key={i}
-                  fill={'#61BFE4'}
-                  rx={4}
-                  ry={4}
-                  onMouseEnter={() => setHoveredTextTopic(w.topic)}
-                  onMouseOut={() => setHoveredTextTopic('miao')}
-                />
-                <text
-                  x={margins.left + 12}
-                  y={yScaleBars(w.word) + 10}
-                  fill={'black'}
-                  fontSize={12}
-                  textAnchor="end"
-                >
-                  {w.word}
-                </text>
-              </g>
-            ))
-          )}
-        </svg>
-      </div>
-
+    <div style={{ display: 'flex' }}>
       <div>
         <BubbleTitle>Intertopic Distance Map (via multidimensional scaling)</BubbleTitle>
         <svg width={width} height={height}>
@@ -161,6 +128,38 @@ export function PCViz({ width, height }) {
               </text>
             </g>
           ))}
+        </svg>
+      </div>
+      <div>
+        <BarTitle>Top 30 Most Salient terms</BarTitle>
+        <svg width={width} height={height}>
+          {defaultData.map((d) =>
+            d.words.map((w, i) => (
+              <g key={i}>
+                <rect
+                  x={margins.left + 15}
+                  y={yScaleBars(w.word)}
+                  width={xScaleBars(w.count)}
+                  height={10}
+                  key={i}
+                  fill={'#61BFE4'}
+                  rx={4}
+                  ry={4}
+                  onMouseEnter={() => setHoveredTextTopic(w.topic)}
+                  onMouseOut={() => setHoveredTextTopic('miao')}
+                />
+                <text
+                  x={margins.left + 12}
+                  y={yScaleBars(w.word) + 10}
+                  fill={'black'}
+                  fontSize={12}
+                  textAnchor="end"
+                >
+                  {w.word}
+                </text>
+              </g>
+            ))
+          )}
         </svg>
       </div>
     </div>
