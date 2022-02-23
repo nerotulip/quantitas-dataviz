8400066b2bf08fba37e37a40cad515e349cb9691
Mattias <mattias.basso@gmail.com>
2022-02-23 13:09:53 +0100

    unpacked components, logging state

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import { HomeViz } from './HomeViz'
+import { PCViz } from './PCViz'
 
 export class App extends React.Component {
   render() {
-    return <HomeViz />
+    return (
+      <div>
+        <HomeViz />
+        <PCViz width={800} height={900} />
+      </div>
+    )
   }
 }
--- src/components/PCViz.js
+++ src/components/PCViz.js
@@ -2,9 +2,18 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import * as d3 from 'd3'
 
-const [hoveredText, setHoveredText] = useState('default')
+export function PCViz({ width, height }) {
+  const [hoveredTextTopic, setHoveredTextTopic] = useState('miao')
+  console.log(hoveredTextTopic)
+  console.log('IN')
 
-function BarText({ width, height }) {
+  const margins = {
+    top: 30,
+    right: 20,
+    left: 50,
+    bottom: 30,
+  }
+
   const barData = [
     {
       topic: 1,
@@ -39,61 +48,11 @@ function BarText({ width, height }) {
         { word: 'lake', count: Math.floor(Math.random() * 100), topic: 3 },
         { word: 'mountain', count: Math.floor(Math.random() * 100), topic: 4 },
         { word: 'states', count: Math.floor(Math.random() * 100), topic: 5 },
+        { word: 'nicolas', count: Math.floor(Math.random() * 100), topic: 5 },
       ],
     },
   ]
 
-  const margins = {
-    top: 30,
-    right: 20,
-    left: 50,
-    bottom: 30,
-  }
-
-  const xScale = d3.scaleLinear().domain([0, 100]).range([0, 600])
-  const yScale = d3
-    .scaleOrdinal()
-    .domain(['car', 'white', 'daisy', 'great', 'gin', 'new york', 'gatsby'])
-    .range(d3.range(margins.top, 300, 20))
-
-  console.log(barData.map((d) => d.words.map((w) => yScale(w.word))))
-  console.log(yScale('white'))
-  console.log(xScale(87))
-  return (
-    <div>
-      <BarTitle>Top 30 Most Salient terms</BarTitle>
-      <svg width={width} height={height}>
-        {barData.map((d) =>
-          d.words.map((w, i) => (
-            <g key={i}>
-              <rect
-                x={margins.left + 15}
-                y={yScale(w.word)}
-                width={xScale(w.count)}
-                height={10}
-                key={i}
-                fill={'#61BFE4'}
-                rx={4}
-                ry={4}
-              />
-              <text
-                x={margins.left + 12}
-                y={yScale(w.word) + 10}
-                fill={'black'}
-                fontSize={12}
-                textAnchor="end"
-              >
-                {w.word}
-              </text>
-            </g>
-          ))
-        )}
-      </svg>
-    </div>
-  )
-}
-
-function Bubbles({ width, height }) {
   const bubbleData = [
     { topic: 1, sizeArea: 10, pc1: 7, pc2: 13 },
     { topic: 2, sizeArea: 4, pc1: 2, pc2: -5 },
@@ -104,68 +63,111 @@ function Bubbles({ width, height }) {
     { topic: 7, sizeArea: 15, pc1: 3, pc2: 10 },
   ]
 
-  const margins = {
-    top: 30,
-    right: 20,
-    left: 20,
-    bottom: 30,
-  }
-
-  const xScale = d3
+  const xScaleBubble = d3
     .scaleLinear()
     .domain([-20, 20])
     .range([margins.left, width - margins.right])
-  const yScale = d3
+  const yScaleBubble = d3
     .scaleLinear()
     .domain([20, -20])
     .range([margins.top, height - margins.bottom])
   const circleScale = d3.scaleSqrt().domain([1, 20]).range([10, 50])
 
-  //   const axesY = d3.axisBottom(xScale)
+  const xScaleBars = d3.scaleLinear().domain([0, 100]).range([0, 600])
+  const yScaleBars = d3
+    .scaleOrdinal()
+    .domain(['car', 'white', 'daisy', 'great', 'gin', 'new york', 'gatsby'])
+    .range(d3.range(margins.top, 300, 20))
 
+  console.log(barData.map((d) => d.words.map((w) => yScaleBars(w.word))))
+  console.log(yScaleBars('white'))
+  console.log(xScaleBars(87))
   return (
+    // BARS DIV
     <div>
-      <BubbleTitle>Intertopic Distance Map (via multidimensional scaling)</BubbleTitle>
-      <svg width={width} height={height}>
-        <line
-          x1={margins.left}
-          y1={height / 2}
-          x2={width - margins.right}
-          y2={height / 2}
-          stroke="#BDBDBD"
-          opacity={0.8}
-        />
-        <line
-          x1={width / 2}
-          y1={margins.top}
-          x2={width / 2}
-          y2={height - margins.bottom}
-          stroke="#BDBDBD"
-          opacity={0.8}
-        />
-        {bubbleData.map((d, i) => (
-          <g key={i}>
-            <circle
-              cx={xScale(d.pc1)}
-              cy={yScale(d.pc2)}
-              r={circleScale(d.sizeArea)}
-              fill={'#61BFE4'}
-              opacity={0.5}
-            />
-            <text
-              x={xScale(d.pc1)}
-              y={yScale(d.pc2) + 4}
-              textAnchor="middle"
-              style={{ 'text-aling': 'middle' }}
-            >
-              {d.topic}
-            </text>
-          </g>
-        ))}
-      </svg>
+      <div>
+        <BarTitle>Top 30 Most Salient terms</BarTitle>
+        <svg width={width} height={height}>
+          {defaultData.map((d) =>
+            d.words.map((w, i) => (
+              <g key={i}>
+                <rect
+                  x={margins.left + 15}
+                  y={yScaleBars(w.word)}
+                  width={xScaleBars(w.count)}
+                  height={10}
+                  key={i}
+                  fill={'#61BFE4'}
+                  rx={4}
+                  ry={4}
+                  onMouseEnter={() => setHoveredTextTopic(w.topic)}
+                  onMouseOut={() => setHoveredTextTopic('miao')}
+                />
+                <text
+                  x={margins.left + 12}
+                  y={yScaleBars(w.word) + 10}
+                  fill={'black'}
+                  fontSize={12}
+                  textAnchor="end"
+                >
+                  {w.word}
+                </text>
+              </g>
+            ))
+          )}
+        </svg>
+      </div>
+
+      <div>
+        <BubbleTitle>Intertopic Distance Map (via multidimensional scaling)</BubbleTitle>
+        <svg width={width} height={height}>
+          <line
+            x1={margins.left}
+            y1={height / 2}
+            x2={width - margins.right}
+            y2={height / 2}
+            stroke="#BDBDBD"
+            opacity={0.8}
+          />
+          <line
+            x1={width / 2}
+            y1={margins.top}
+            x2={width / 2}
+            y2={height - margins.bottom}
+            stroke="#BDBDBD"
+            opacity={0.8}
+          />
+          {bubbleData.map((d, i) => (
+            <g key={i}>
+              <circle
+                cx={xScaleBubble(d.pc1)}
+                cy={yScaleBubble(d.pc2)}
+                r={circleScale(d.sizeArea)}
+                fill={'#61BFE4'}
+                opacity={
+                  hoveredTextTopic === 'default' ? 0.5 : d.topic === hoveredTextTopic ? 0.5 : 0.2
+                }
+              />
+              <text
+                x={xScaleBubble(d.pc1)}
+                y={yScaleBubble(d.pc2) + 4}
+                textAnchor="middle"
+                style={{ 'text-aling': 'middle' }}
+                opacity={
+                  hoveredTextTopic === 'default' ? 0.5 : d.topic === hoveredTextTopic ? 0.5 : 0.2
+                }
+              >
+                {d.topic}
+              </text>
+            </g>
+          ))}
+        </svg>
+      </div>
     </div>
   )
 }
+
+// STYLED COMPONENTS
 
 const BubbleTitle = styled.div`
   font-size: 18px;
