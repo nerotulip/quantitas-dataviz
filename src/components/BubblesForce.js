dbe935b244298de8647a0ec58ba48870aca2d90e
Mattias <mattias.basso@gmail.com>
2022-02-25 12:22:33 +0100

    force with scales, before change

--- src/components/BubblesForce.js
+++ src/components/BubblesForce.js
@@ -23,35 +23,44 @@ export function BubblesForce({ width, height }) {
       nReviews: 8,
       LivingLab: 'The Scheldeland region',
     },
+    {
+      clusterID: 0,
+      macro: 'Natural Sites',
+      nReviews: 4,
+      LivingLab: 'The Scheldeland region',
+    },
+    {
+      clusterID: 0,
+      macro: 'Shopping',
+      nReviews: 1,
+      LivingLab: 'The Scheldeland region',
+    },
+    {
+      clusterID: 0,
+      macro: 'Relax and Wellness',
+      nReviews: 12,
+      LivingLab: 'The Scheldeland region',
+    },
   ]
 
-  const xScale = d3.scaleOrdinal().domain([]).range([200, 300])
+  const xScale = d3.scaleOrdinal().domain([]).range([400, 600])
 
-  const yScale = d3.scaleLinear().domain([0, 200]).range([100, 300])
+  const yScale = d3.scaleLinear().domain([0, 200]).range([200, 400])
 
-  const circleScale = d3.scaleSqrt().domain([1, 20]).range([10, 50])
+  const circleScale = d3.scaleSqrt().domain([1, 20]).range([5, 60])
 
   const radius = 10
 
   const simulation = d3
     .forceSimulation(forceData)
+    .force('charge', d3.forceManyBody().strength(5))
+    .force('center', d3.forceCenter(width / 2, height / 2))
     .force(
-      'x',
-      d3
-        .forceX(function (d) {
-          return xScale(d.macro)
-        })
-        .strength(0.03)
+      'collision',
+      d3.forceCollide().radius(function (d) {
+        return circleScale(d.nReviews)
+      })
     )
-    .force(
-      'y',
-      d3
-        .forceY(function (d) {
-          return yScale(d.nReviews)
-        })
-        .strength(0.09)
-    )
-    .force('collide', d3.forceCollide(12))
 
   //  Manually run simulation
   simulation.tick(10)
@@ -68,6 +77,7 @@ export function BubblesForce({ width, height }) {
               cy={d.y}
               fill={'white'}
               stroke={'black'}
+              strokeWidth={3}
             />
           ))}
         </g>
